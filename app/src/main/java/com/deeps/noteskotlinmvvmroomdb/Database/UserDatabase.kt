5f940649c9d196cf893874af2572115359f1d69a
deeps <deepshikha6464@gmail.com>
2021-09-12 15:55:05 +0530

    database created

---
+++ app/src/main/java/com/deeps/noteskotlinmvvmroomdb/Database/UserDatabase.kt
@@ -0,0 +1,37 @@
+package com.deeps.noteskotlinmvvmroomdb.Database
+
+import android.content.Context
+import androidx.room.Dao
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.deeps.noteskotlinmvvmroomdb.Model.User
+import kotlinx.coroutines.internal.synchronized
+
+@Database(entities = [User::class],version = 1,exportSchema = false)
+abstract class UserDatabase : RoomDatabase() {
+
+
+    abstract  fun getDao():Dao            //will call all the function of dao interface in repository
+
+    companion object {
+        private const val DATABASE_NAME = "UserDatabase"
+        @Volatile
+        var instance:UserDatabase ?=null
+        fun getInstance(context: Context):UserDatabase?
+        {
+            if(instance==null)
+            {
+                 kotlin.synchronized(UserDatabase::class.java)
+                 {
+                    if(instance== null)
+                    {
+                        instance = Room.databaseBuilder(context,UserDatabase::class.java,
+                            DATABASE_NAME).build()
+                    }
+                }
+            }
+            return instance
+        }
+    }
+}
