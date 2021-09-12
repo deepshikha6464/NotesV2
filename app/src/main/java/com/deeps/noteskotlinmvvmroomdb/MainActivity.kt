c556aec4852406899fec73596f4138e65fc9813b
deeps <deepshikha6464@gmail.com>
2021-09-12 16:39:15 +0530

    calling viewmodel in mainactivity

--- app/src/main/java/com/deeps/noteskotlinmvvmroomdb/MainActivity.kt
+++ app/src/main/java/com/deeps/noteskotlinmvvmroomdb/MainActivity.kt
@@ -2,10 +2,15 @@ package com.deeps.noteskotlinmvvmroomdb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.deeps.noteskotlinmvvmroomdb.ViewModel.UserviewModel
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var viewModel:UserviewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        viewModel = ViewModelProvider(this).get(UserviewModel::class.java)
     }
 }
